8b7d4335e9b15031bb56faf7c5e5c810c54f3297
Gtacer <459472218@qq.com>
2018-01-01 22:42:03 +0800

    路由类

--- system/core/Router.php
+++ system/core/Router.php
@@ -2,7 +2,20 @@
 
 namespace Tracer\system\core;
 
+use function Tracer\system\core\Common\load_class;
+use Tracer\application\controller;
+
 class Router
 {
+    /**
+     * Router constructor.
+     */
+    public function __construct()
+    {
+        $_url = load_class('url');
+        $class = 'Tracer\application\controller\\' . $_url->class;
+        $obj = new $class;
+        call_user_func_array([$obj,'hello'],$_url->param);
+    }
 
 }
