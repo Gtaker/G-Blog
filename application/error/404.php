347e21caee13b3168463d4f030134a0ca86b9d5b
Gtacer <459472218@qq.com>
2018-01-02 22:35:52 +0800

    404错误提示界面

---
+++ application/error/404.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <!-- 引入样式 -->
+    <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <script src="https://unpkg.com/element-ui/lib/index.js"></script>
+</head>
+<body>
+<div id="card">
+    <el-card class="box-card">
+        <div slot="header" class="clearfix">
+            <span class="title">404 NOT FOUND</span>
+        </div>
+        <div class="text item">
+            请求的页面不存在！
+        </div>
+    </el-card>
+</div>
+</body>
+
+<style>
+    #card {
+        margin: 0 auto;
+        width: 880px;
+        height: 100px;
+    }
+
+    .title {
+        font-weight: bold;
+        font-size: 38px;
+    }
+
+    .text {
+        font-size: 28px;
+        color: #909399;
+    }
+
+    .item {
+        margin-bottom: 18px;
+    }
+    .clearfix {
+        text-align: center;
+    }
+
+    .clearfix:before,
+    .clearfix:after {
+        display: table;
+        content: "";
+    }
+
+    .clearfix:after {
+        clear: both
+    }
+
+    .box-card {
+        text-align: center;
+        width: 880px;
+        line-height: 45px;
+    }
+</style>
+<script>
+    new Vue({
+        el: '#card',
+        data: function () {
+            return {
+                visible: false
+            }
+        }
+    })
+</script>
+</html>
