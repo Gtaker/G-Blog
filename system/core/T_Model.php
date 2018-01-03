1242ad09e87dbbd957c709327363ac1a9228ff57
Gtacer <459472218@qq.com>
2018-01-03 16:18:45 +0800

    在构造函数中连接数据库

--- system/core/T_Model.php
+++ system/core/T_Model.php
@@ -3,13 +3,23 @@
 namespace Tracer\system\core;
 
 
+use function Tracer\system\core\Common\load_class;
+
 class T_Model
 {
+    protected $connect;
+
     /**
      * T_Model constructor.
      */
     public function __construct()
     {
+        $_db =& load_class('Config');
+        $this->connect = new \PDO($_db->getItem('dsn'), $_db->getItem('db_username'), $_db->getItem('db_psd'));
+    }
 
+    public function getConnect(): \PDO
+    {
+        return $this->connect;
     }
 }
