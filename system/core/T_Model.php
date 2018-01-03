98146df991ee4cd5ee9501e312223ab8ec68a31a
Gtacer <459472218@qq.com>
2018-01-03 22:53:31 +0800

    解决PDO默认抛出GBK编码的异常信息的问题

--- system/core/T_Model.php
+++ system/core/T_Model.php
@@ -15,7 +15,16 @@ class T_Model
     public function __construct()
     {
         $_db =& load_class('Config');
-        $this->connect = new \PDO($_db->getItem('dsn'), $_db->getItem('db_username'), $_db->getItem('db_psd'));
+        try {
+            $this->connect = new \PDO(
+                $_db->getItem('dsn'),
+                $_db->getItem('db_username'), $_db->getItem('db_psd'),
+                [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_TIMEOUT => 3]);
+        } catch (\PDOException $exception) {
+            throw new \Exception(
+                iconv('gbk', 'utf-8', $exception->getMessage()),
+                $exception->getCode());
+        }
     }
 
     public function getConnect(): \PDO
