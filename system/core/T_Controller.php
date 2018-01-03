b1cdf35b368a9ad7fdf77c6cedc1d9209367e45b
Gtacer <459472218@qq.com>
2018-01-03 16:18:23 +0800

    增加view方法，调用视图

--- system/core/T_Controller.php
+++ system/core/T_Controller.php
@@ -4,5 +4,26 @@ namespace Tracer\system\core;
 
 class T_Controller
 {
-    
+    /**
+     * T_Controller constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * 调用视图文件
+     * @param string $view
+     * @param array  $data
+     */
+    public function view(string $view, array &$data = []): void
+    {
+        ob_clean();
+        extract($data);
+
+        require VIEW_PATH . DIRECTORY_SEPARATOR ."$view.php";
+        exit(0);
+    }
+
+
 }
